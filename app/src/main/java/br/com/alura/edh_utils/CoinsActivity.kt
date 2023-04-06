eaea5e541eb667880d2ec7a5561731c8fd1e52e6
dcoutinhu <diegoengcontrole@gmail.com>
2023-04-06 17:55:46 -0300

    Commit for #7. Close #7

--- app/src/main/java/br/com/alura/edh_utils/CoinsActivity.kt
+++ app/src/main/java/br/com/alura/edh_utils/CoinsActivity.kt
@@ -3,6 +3,7 @@ package br.com.alura.edh_utils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.alura.edh_utils.databinding.ActivityCoinsBinding
+import kotlin.random.Random
 
 class CoinsActivity : AppCompatActivity() {
 
@@ -17,12 +18,22 @@ class CoinsActivity : AppCompatActivity() {
     }
 
     private fun onButtonPress() {
+        binding.coinsActivityFlipButton.setOnClickListener {
+            val randomNumber : Int = (1..2).random()
+
+            if (randomNumber == 1) {
+                flipTheCoin(R.drawable.iv_heads,"Heads!")
+            }
+            else {
+                flipTheCoin(R.drawable.iv_tails,"Tails!")
+            }
+        }
     }
 
     private fun flipTheCoin(imageId: Int, coinSide: String) {
         binding.ivCoin.animate().apply {
             duration = 1000
-            rotationYBy(1000F)
+            rotationYBy(1800F)
             binding.ivCoin.isClickable = false
         }.withEndAction {
             binding.ivCoin.setImageResource(imageId)
